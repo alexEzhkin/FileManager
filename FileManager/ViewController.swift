4b2dea33823ddd16e57a7df5ed3c66682f491e2a
Alex Yozhkin- <>
2022-06-01 00:18:01 +0300

    Added bar item on viewController to create folder

--- FileManager/ViewController.swift
+++ FileManager/ViewController.swift
@@ -11,9 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addCreateFolderBarItem()
     }
-
+    
+    func addCreateFolderBarItem() {
+        let settingsBarButtonItem = UIBarButtonItem(
+            title: NSString(string: "+") as String,
+            style: .done,
+            target: self,
+            action: #selector(createFolder))
+        let font = UIFont.systemFont(ofSize: 28)
+        let attributes = [NSAttributedString.Key.font : font]
+        settingsBarButtonItem.setTitleTextAttributes(attributes, for: .normal)
+        self.navigationItem.rightBarButtonItem  = settingsBarButtonItem
+    }
+    
+    @objc func createFolder(){
+        print("create Folder")
+    }
 
 }
 
